697a4d61c59967dac1c7174e27147f973cd9adb7
Franky Cunza <franky.cunza@prix.tips>
2021-07-01 11:34:47 -0500

    changes

---
+++ android/app/src/main/java/com/veo_app_reacnative/MainActivity.java
@@ -0,0 +1,15 @@
+package com.veo_app_reacnative;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "veo_app_reacnative";
+  }
+}
