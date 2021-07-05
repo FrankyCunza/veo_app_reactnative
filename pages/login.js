fe309a611d4493cb3edc219f63fbfeedd23e8da6
Franky cunza chavez <franky.cunza@gmail.com>
2021-07-05 14:43:08 -0500

    changes

--- pages/login.js
+++ pages/login.js
@@ -6,31 +6,74 @@ const Login = () => {
     const { control, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     return (
-        <View>
-            <Text>Hello Login!</Text>
-            <Controller
-                control={control}
-                rules={{
-                required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                    style={styles.input}
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
+        <View style={{paddingHorizontal: 12}}>
+            <Text style={styles.title}>Iniciar sesión</Text>
+            <View style={styles.item}>
+                <Text style={styles.label}>Usuario:</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                    required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                    )}
+                    name="firstName"
+                    defaultValue=""
                 />
-                )}
-                name="firstName"
-                defaultValue=""
-            />
+            </View>
+            <View style={styles.item, {marginTop: 14}}>
+                <Text style={styles.label}>Contraseña:</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                    required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                    )}
+                    name="firstName"
+                    defaultValue=""
+                />
+            </View>
+            <View style={{marginTop: 16}}>
+                <Button title="Enviar" style={styles.send} onPress={handleSubmit(onSubmit)} />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    label: {
+        fontSize: 16,
+        marginBottom: 6
+    },
+    send: {
+        height: 54
+    },
+    item: {
+
+    },
     input: {
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
+      borderRadius: 5,
+      paddingLeft: 14
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+        fontWeight: "bold",
+        marginVertical: 14
     },
     header: {
       fontSize: 20
