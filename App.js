50fdbd11a6d959465f3ca7c94660ac7753fb041a
Franky cunza chavez <franky.cunza@gmail.com>
2021-08-07 10:01:36 -0500

    changes

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
-import React, { useEffect } from 'react';
-import Video from './pages/video';
-import { Text, View } from 'react-native'
-function App() {
+import React from 'react';
+import { View, Text, StyleSheet, AppRegistry } from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './pages/home';
+import Login from './pages/login';
+import Video from './pages/jitsi';
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <>
-      <Text>Hello</Text>
-      <Video />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Video">
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Overview' }} />
+        {/* <Stack.Screen name="/testdiario" component={Daily} options={{ title: 'Declaración diaria' }} /> */}
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+        <Stack.Screen name="Video" component={Video} options={{ title: 'Video' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <NativeRouter>
+    //   <View style={styles.container}>
+    //     <View style={styles.nav}>
+    //       <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+    //         <Text>Login</Text>
+    //       </Link>
+    //       <Link
+    //         to="/Daily"
+    //         underlayColor="#f0f4f7"
+    //         style={styles.navItem}
+    //       >
+    //         <Text>Daily</Text>
+    //       </Link>
+    //     </View>
+
+    //     <Route exact path="/" component={Login} />
+    //     <Route path="/Daily" component={Daily} />
+    //   </View>
+    // </NativeRouter>
+    // <NavigationContainer>
+    //   <Tab.Navigator>
+    //     <Tab.Screen name="Login" component={Login} />
+    //     <Tab.Screen name="Home" component={Home} />
+    //     <Tab.Screen name="Daily" component={Daily} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f2f2f2"
+  },
+  header: {
+    fontSize: 20
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  },
+  topic: {
+    textAlign: "center",
+    fontSize: 15
+  }
+});
+
+
+// AppRegistry.registerComponent("MyApp", () => App);
 export default App;
---
+++ README.md
@@ -0,0 +1,37 @@
+# Install CLI
+`npm install -g react-native-cli`
+
+# Install packages
+`npm install`
+
+# RUN ANDROID
+`npx react-native run-android`
+or
+`react-native start`
+
+# No Apps Connecting
+  # Reset metro bundler cache : 
+  `npx react-native start --reset-cache`
+      
+  # Remove Android assets cache : 
+  `cd android && ./gradlew clean`
+      
+  # Relaunch metro server : 
+  `npx react-native run-android`
+# View devices
+`adb devices`
+
+# Async storage
+https://react-native-async-storage.github.io/async-storage/docs/usage
+
+# Type annotations can only be used in TypeScript files.ts(8010) on React-Native CLI Startup
+1. Set "javascript.validate.enable": false in your VS Code settings.
+
+# If you sure the module exists, try these steps:
+1. Clear Watchman watches: watchman watch-del-all
+2. Delete node_modules and run yarn install
+3. Reset Metro's cache: yarn start --reset-cache
+4. Remove the cache: rm -rf /tmp/metro-*
+
+# Watchman
+https://github.com/facebook/watchman/releases/tag/v2020.08.17.00
