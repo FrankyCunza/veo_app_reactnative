abda4761cfd334a14efb46fd91e840e8a26735ac
Franky cunza chavez <franky.cunza@gmail.com>
2021-08-06 21:15:29 -0500

    changes

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import Video from './pages/video';
+import { Text, View } from 'react-native'
+function App() {
+  return (
+    <>
+      <Text>Hello</Text>
+      <Video />
+    </>
+  )
+}
+export default App;
